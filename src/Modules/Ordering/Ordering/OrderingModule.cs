c411deebc5f188ebe050b34c80bd7e2e4130fa59
MichenFCMB <michen.jesumichen@fcmb.com>
2025-12-29 18:44:56 +0100

    .

---
+++ src/Modules/Ordering/Ordering/OrderingModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering
+{
+    public static class OrderingModule
+    {
+        public static IServiceCollection AddOrderingModule(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseOrderingModule(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
