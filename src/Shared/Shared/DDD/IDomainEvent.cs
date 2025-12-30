e9299fdfa60917cbfbff4ea5f0f9be72e7c5573e
MichenFCMB <michen.jesumichen@fcmb.com>
2025-12-30 20:53:30 +0100

    add domain events

---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Shared.DDD
+{
+    public interface IDomainEvent: INotification
+    {
+        public Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName!;
+
+    }
+}
