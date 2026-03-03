e9d02ae6bec6e0a81198d3e6e7fc321e0db80b49
MichenFCMB <michen.jesumichen@fcmb.com>
2026-03-03 10:21:19 +0100

    .

---
+++ src/Shared/Shared/Data/Interceptors/DispatchDomainEventInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Shared.DDD;
+
+namespace Shared.Data.Interceptors
+{
+    public class DispatchDomainEventInterceptor(Mediator mediator): SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            DispatchDomainEvents(eventData.Context).GetAwaiter().GetResult();
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            await DispatchDomainEvents(eventData.Context);
+            return await base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private async Task DispatchDomainEvents(DbContext context)
+        {
+            if (context == null) return;
+
+            var aggregate = context.ChangeTracker.Entries<IAggregate>().Where(e => e.Entity.DomainEvents != null && e.Entity.DomainEvents.Any())
+                .Select(e => e.Entity)
+                .ToList();
+
+            var domainEvents = aggregate.SelectMany(a => a.DomainEvents).ToList();
+
+            aggregate.ToList().ForEach(a => a.ClearDomainEvents());
+
+            foreach ( var domainEvent in domainEvents)
+                await mediator.Publish(domainEvent);
+        }
+
+    }
+}
